d6fcc9fd399cc9d390ad0f0411e587292fb12ce7
Hensha-w <himtwo024@gmail.com>
2025-11-06 11:26:29 -0800

    first node.js code completed

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
+
 
 //express app
 const app = express();
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs');
 
 //middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 //mongoose and mongo sandbox routes
@@ -80,22 +82,14 @@ res.render('about', { title: 'About' });
 });
 
 //blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}); 
+app.use('/blogs', blogRoutes);
 
 
-app.get('/create', (req, res) => {
-//   res.send('Create a new blog');
-// res.render('create');
-    res.render('create', { title: 'Create a new Blog' });
-});
+// app.get('/create', (req, res) => {
+// //   res.send('Create a new blog');
+// // res.render('create');
+//     // res.render('create', { title: 'Create a new Blog' });
+// });
 
 //redirects
 app.get('/about-us', (req, res) => {
